3d00a26ba884e127828af6e89f93872a77607e79
Kareem-Krayem <kareemkrayem.w@gmail.com>
2020-10-03 01:09:49 +0300

    videos-added

--- tiktok-clone-react/src/App.js
+++ tiktok-clone-react/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Video from './components/Video/Video';
 
 function App() {
+  const video1 = require('./assets/01.MP4');
+  const video2 = require('./assets/02.MP4');
+  const video3 = require('./assets/03.MP4');
+  const video4 = require('./assets/04.MP4');
   return (
     <div className='app'>
-      <h1>TikTok Clone Using React</h1>
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video vid={video1} />
+        <Video vid={video2} />
+        <Video vid={video3} />
+        <Video vid={video4} />
       </div>
     </div>
   );
--- tiktok-clone-react/src/components/Video/Video.js
+++ tiktok-clone-react/src/components/Video/Video.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Video.css';
 import VideoSideBar from '../VideoSideBar/VideoSideBar';
 import VideoFooter from '../VideoFooter/VideoFooter';
 
-function Video() {
+function Video({ vid }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className='video'>
-      <h1>Video</h1>
+      <video
+        className='video__player'
+        onClick={onVideoPress}
+        ref={videoRef}
+        loop
+        src={vid}
+      ></video>
       <VideoSideBar />
       <VideoFooter />
     </div>
