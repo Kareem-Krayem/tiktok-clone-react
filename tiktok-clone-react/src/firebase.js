4b75896ca82a785d636c2d050e2dfbfa253062f7
Kareem-Krayem <kareemkrayem.w@gmail.com>
2020-10-06 13:04:59 +0300

    init

---
+++ tiktok-clone-react/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCEwT2oZec0nX4dLXIvSm5y71PQoqlD8EA',
+  authDomain: 'tiktok-clone-24b8d.firebaseapp.com',
+  databaseURL: 'https://tiktok-clone-24b8d.firebaseio.com',
+  projectId: 'tiktok-clone-24b8d',
+  storageBucket: 'tiktok-clone-24b8d.appspot.com',
+  messagingSenderId: '688765422073',
+  appId: '1:688765422073:web:89fc4609820a924a929528',
+};
